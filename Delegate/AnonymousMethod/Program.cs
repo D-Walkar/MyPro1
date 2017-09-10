38bdfe5f5088a43c2521561bb97a0fed68950b48
DWalkar <30808336+DWalkar@users.noreply.github.com>
2017-09-10 19:17:45 +0530

    Add files via upload

---
+++ Delegate/AnonymousMethod/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonymousMethod
+{
+    class Program
+    {
+       // public delegate void mydel();
+        public delegate int mydeleg(int a,int b);
+         
+        static void Main(string[] args)
+        {
+         /*   mydel d1 = delegate ()
+            {
+                Console.WriteLine("Deepak");
+            };
+            d1();
+            */
+
+            //Anonnymous Method
+
+            mydeleg d2 = (a,b) => (a+b);
+            Console.WriteLine(d2(100, 200));
+            Console.Read();
+
+        }
+    }
+}
---
+++ Delegate/Delegate/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    class Program
+    {
+        public delegate void mydel(int a, int b);
+        class ClsInfo
+        {
+            public void GetVal(int a,int b)
+            {
+                Console.WriteLine(a+b);
+            }
+        }
+        static void Main(string[] args)
+        {
+            ClsInfo obj = new ClsInfo();
+            mydel d1 = new mydel(obj.GetVal);
+            d1(10, 20);
+            Console.Read();
+        }
+    }
+}
